b0c9bbb099f8b6785be285deeefa91e67ea41247
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-02-23 13:47:24 -0300

    Conceito de middlewares

--- streams/buffer.js
+++ streams/buffer.js
@@ -3,7 +3,7 @@
 //Representação de um espaço da memória no computador, maneiras de salvar e ler da memória de forma perfomática
 //perfomática de maneira binária
 
-const buf = Buffer.from('ok'); //<Buffer 6f 6b>
+const buf = Buffer.from('ok'); //<Buffer 6f 6b> //Hexadecimal
 
 console.log(buf);
-console.log(buf.toJSON());
+console.log(buf.toJSON()); //{ type: 'Buffer', data: [ 111, 107 ] } Decimal
