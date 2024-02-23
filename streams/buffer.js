217def6a61a0536295b6d499f73901685bd44b7b
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-02-23 13:42:26 -0300

    Buffers

---
+++ streams/buffer.js
@@ -0,0 +1,9 @@
+//Buffers
+
+//Representação de um espaço da memória no computador, maneiras de salvar e ler da memória de forma perfomática
+//perfomática de maneira binária
+
+const buf = Buffer.from('ok'); //<Buffer 6f 6b>
+
+console.log(buf);
+console.log(buf.toJSON());
