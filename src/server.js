9a95cf79847cdfddf1a68767b707cf18d5153719
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-02-24 00:31:50 -0300

    Local database creation

--- src/server.js
+++ src/server.js
@@ -34,21 +34,26 @@ CRUD
     CLIENT ERROR (400-499) -> Erros geradas na requisição do Front-end
     SERVER ERROR (500-599) -> Erros gerados na response do Back-end
   */
-const users = [];
+
 import { json } from './middlewares/json.js';
+import { Database } from './database.js';
+
+const database = new Database();
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
   await json(req, res);
   if (method == 'GET' && url == '/users') {
+    const users = database.select('users');
     return res.end(JSON.stringify(users));
   }
   if (method === 'POST' && url == '/users') {
     const { name, email } = req.body;
-    users.push({
+    const user = {
       id: 1,
       nome: name,
       email: email,
-    });
+    };
+    database.insert('users', user);
     return res.writeHead(201).end();
   }
   return res.writeHead(404).end('Not Found'); //Error 404 - Qualquer rota que não exista
